c7c6fa2cc80bde4a617e26e43d31b9c1d6d6de70
Katherine Tam <katherinemtam@gmail.com>
2021-07-30 14:51:37 -0700

    pass id to listitem

--- src/components/history/History.jsx
+++ src/components/history/History.jsx
@@ -1,14 +1,17 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 
-const History = ({ search, method }) => (
+const History = ({ id, search, method }) => (
   <>
-    <p>{method}</p>
-    <p>{search}</p>
+    <article id={id}>
+      <p id={id}>{method}</p>
+      <p id={id}>{search}</p>
+    </article>)
   </>
 );
 
 History.propTypes = {
+  id: PropTypes.string.isRequired,
   search: PropTypes.string.isRequired,
   method: PropTypes.string.isRequired,
 };
