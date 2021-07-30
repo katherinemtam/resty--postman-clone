7072ca653fcd2bbad84a7831538ea98fd5c03a90
Katherine Tam <katherinemtam@gmail.com>
2021-07-30 14:51:28 -0700

    pass onClick to list

--- src/components/history/HistoryList.jsx
+++ src/components/history/HistoryList.jsx
@@ -2,17 +2,20 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import History from './History';
 
-const HistoryList = ({ history }) => {
-
+const HistoryList = ({ history, onClick }) => {
   const historyElements = history.map((eachHistory, index) => (
-    <li key={index}>
+    <li 
+      key={index}
+      onClick={onClick}
+    >
       <History 
+        id={`${eachHistory.search}${eachHistory.method}${eachHistory.body}`}
         search={eachHistory.search}
         method={eachHistory.method}
       />
     </li>
   ));
-  
+
   return <ul>{historyElements}</ul>;
 };
 
@@ -22,7 +25,8 @@ HistoryList.protoTypes = {
       search: PropTypes.string.isRequired,
       method: PropTypes.string.isRequired,
     })
-  ),
+  ).isRequired,
+  onClick: PropTypes.func.isRequired
 };
 
 export default HistoryList;
