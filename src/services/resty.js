b351bb6bcfc56a2150222c4cbf5c025fad3cc0d4
Katherine Tam <katherinemtam@gmail.com>
2021-08-02 15:03:02 -0700

    conditional for GET or DELETE methods

--- src/services/resty.js
+++ src/services/resty.js
@@ -1,10 +1,17 @@
+const WITHOUT_BODY_METHODS = ['GET', 'DELETE'];
+
 export const fetchApi = async (search, method, body) => {
+
+  const headers = WITHOUT_BODY_METHODS.includes(method)
+    ? {}
+    : { 'Content-Type': 'application/json' };
+
   const res = await fetch(search, { 
     method,
-    headers: {
-      'Content-Type': 'application/json'
-    },
-    body 
+    headers,
+    body: WITHOUT_BODY_METHODS.includes(method)
+      ? null
+      : body
   });
 
   const json = await res.json();
